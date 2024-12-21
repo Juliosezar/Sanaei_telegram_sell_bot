78171fdd30865cc8337b02d006df0a4c293b3404
Sezar <julio.sezar.1382@gmail.com>
2024-12-21 16:12:53 +0330

    bug fix / add iphon

--- configs/views.py
+++ configs/views.py
@@ -282,7 +282,7 @@ class Sublink(APIView):
                 content_str += (i + "\n")
             user_agent = request.headers.get('User-Agent', None)
             print(user_agent)
-            is_v2ray_client = any(word in user_agent for word in ["hiddify", "v2ray", "Streisand", "Hiddify", "V2ray"])
+            is_v2ray_client = any(word in user_agent for word in ["hiddify", "v2ray", "Streisand", "Hiddify", "V2ray", "V2Box", "FoXray"])
             if is_v2ray_client:
                 service_obj = Service.objects.get(uuid=config_uuid)
                 time_stamp = datetime.now().timestamp()
