7b3bf61c4485507b558d37438a8f5776d601a785
Sezar <julio.sezar.1382@gmail.com>
2025-02-23 23:31:55 +0330

    bog fix

--- SanaeiBot/celery_conf.py
+++ SanaeiBot/celery_conf.py
@@ -10,6 +10,6 @@ celery_app = Celery('SanaeiBot')
 celery_app.conf.broker_connection_retry_on_startup = True
 celery_app.autodiscover_tasks()
 celery_app.conf.broker_url = environ.get("CELERY_BROKER_URL")
-celery_app.conf.result_backend = 'rpc://'
+celery_app.conf.result_backend = environ.get("CELERY_RESULT_BACKEND")
 celery_app.conf.task_serializer = 'json'
 # celery_app.conf.result_expires = timedelta(hours=1)
